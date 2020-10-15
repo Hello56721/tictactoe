ca7df95c52539b818eee3218755a14d836c10c28
Hello56721 <Hello56721@github.com>
2020-10-15 14:36:34 -0600

    Added multiplayer system

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,34 @@
 
 #include "board.h"
 
+struct pos {
+    int row;
+    int colum;
+};
+
+pos getTurn() {
+    std::cout << "which row: ";
+    int row;
+    std::cin >> row;
+    std::cout << "which colum: ";
+    int colum;
+    std::cin >> colum;
+    
+    return {row, colum};
+}
+
 int main() {
     std::cout << "Welcome to Yue's Terminal based TicTacToe Game!\n";
     char shouldQuit;
     while (shouldQuit != 'y') {
         std::cout << "Player 1, your turn" << std::endl;
-        std::cout << "which row: ";
-        int row;
-        std::cin >> row;
-        std::cout << "which colum: ";
-        int colum;
-        std::cin >> colum;
+        pos player1 = getTurn();
+        board::positions[player1.row][player1.colum] = "X";
+        board::print();
         
-        board::positions[row][colum] = "X";
+        std::cout << "Player 2, your turn" << std::endl;
+        pos player2 = getTurn();
+        board::positions[player2.row][player2.colum] = "O";
         board::print();
         
         std::cout << "Quit?(y/n) ";
